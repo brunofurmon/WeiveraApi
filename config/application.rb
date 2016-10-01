6dae9abcaaa5483f13745f0f2f309a1a1989b726
Bruno Oliveira <brunofurmon@poli.ufrj.br>
2016-10-01 15:21:25 -0300

    Changed "Rack:Cors" to Rack:Cors

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module WeiveraApi
     config.api_only = true
 
     # Inserts rack_cors before any other middleware
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
         allow do
             origins '*'
             resource '*', :headers => :any, :methods => [:get, :post, :options]
